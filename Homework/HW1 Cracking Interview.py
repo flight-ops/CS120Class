84b79a4da4df0be43444852e0b09917a9ca24e1c
flight-ops <ezhou6@gmail.com>
2023-09-11 14:35:05 -0400

    just commit

--- Homework/HW1 Cracking Interview.py
+++ Homework/HW1 Cracking Interview.py
@@ -1,9 +1,18 @@
 #create variable to store string to be operated on
 operand_string = "abbccddeeee"
+
+operand_list = list(operand_string)
+print(operand_list)
+
 #create variable to store final result
-operand_set=set(operand_string)
-final_string = None
+
+count_of_letter = operand_list.count()
+print(count_of_letter)
 
 #use a for loop 
-
+for letter in operand_string:
+    currentindex=0
+    #insert number of a given letter in the correct index
+    operand_list.insert(currentindex+1,str(count_of_letter))
+    print(operand_list)
 #end result should be "ab2c2d2e4" 
