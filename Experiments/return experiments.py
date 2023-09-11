c99729ae98b9aa9a864cbd9a898df2d2058ecb72
flight-ops <ezhou6@gmail.com>
2023-09-11 16:23:20 -0400

    doing things in class

---
+++ Experiments/return experiments.py
@@ -0,0 +1,11 @@
+addition = 0
+def adding_integers (a,b):
+    addition = a+b
+    return addition
+
+looping=0
+
+while looping == 0:
+    a = float(input("first number?"))
+    b = float(input("second number?"))
+    print(addition)
