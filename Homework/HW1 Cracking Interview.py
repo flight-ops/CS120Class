14f28b43f913515269e51fe55e67ce56f3de7b85
flight-ops <ezhou6@gmail.com>
2023-09-11 14:17:27 -0400

    trying to do homework ahead of time

---
+++ Homework/HW1 Cracking Interview.py
@@ -0,0 +1,9 @@
+#create variable to store string to be operated on
+operand_string = "abbccddeeee"
+#create variable to store final result
+operand_set=set(operand_string)
+final_string = None
+
+#use a for loop 
+
+#end result should be "ab2c2d2e4" 
