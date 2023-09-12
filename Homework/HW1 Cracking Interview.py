201c83e2764efbea72134d1c849bdcf65265513c
flight-ops <ezhou6@gmail.com>
2023-09-12 08:19:36 -0400

    back to square one

--- Homework/HW1 Cracking Interview.py
+++ Homework/HW1 Cracking Interview.py
@@ -6,13 +6,13 @@ print(operand_list)
 
 #create variable to store final result
 
-count_of_letter = operand_list.count()
-print(count_of_letter)
 
-#use a for loop 
-for letter in operand_string:
-    currentindex=0
-    #insert number of a given letter in the correct index
-    operand_list.insert(currentindex+1,str(count_of_letter))
-    print(operand_list)
+index = 0
+
+
+#use a for loop to run through all of the unique letters
+while index <= 10:
+    print (index)
+    index+=1
+
 #end result should be "ab2c2d2e4" 
