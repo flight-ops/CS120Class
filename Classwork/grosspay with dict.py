3d42a101241e00c1abc669dbf820702e14a0e559
flight-ops <ezhou6@gmail.com>
2023-09-18 15:28:45 -0400

    functions today

--- Classwork/grosspay with dict.py
+++
@@ -1,6 +0,0 @@
-grosspay_dict = {
-    "Name":input("What is your name?"),
-    "Hourly Pay":input("What is your hourly wage?"),
-    "Hours Count":input("How many hours did you work?"),
-}
-print(grosspay_dict)
