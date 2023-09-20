00926a8f10240e112adc245755679208ea6cd7fe
flight-ops <ezhou6@gmail.com>
2023-09-20 17:11:43 -0400

    renamed something

---
+++ Homework/HW1 Cracking Interview with Sets.py
@@ -0,0 +1,15 @@
+#main function
+
+def main ():
+    input_string = "hurray"
+    operand_set = set(input_string)
+    
+
+    for i in range (len(input_string)):
+        print(operand_set)
+    
+
+
+
+
+main()
