0305a2066b892d07bd5e5d95dd866e0a8f200d73
flight-ops <ezhou6@gmail.com>
2023-09-20 15:50:17 -0400

    successful compression

--- Homework/HW1 Cracking Interview.py
+++ Homework/HW1 Cracking Interview.py
@@ -1,31 +1,36 @@
-#create variable to store string to be operated on, or maybe catch a user's input
-operand_string = "abbccddeeee"
-#create a list for all of the letters in the alphabet
-alphabet_container_list = list("abcdefghijklmnopqrstuvwxyz")
+#main function
+def main():
+    #create variable to store string to be operated on, or maybe catch a user's input
+    operand_string = "abbccddeeee"
+    #create a list for all of the letters in the alphabet
+    alphabet_container_list = list("abcdefghijklmnopqrstuvwxyz")
 
-#print for debugging, remove later
-#print(alphabet_container_list)
+    #convert the string provided either by default or by the user to a list or set
+    operand_list = list(operand_string)
 
-#convert the string provided either by default or by the user to a list that can be read one by one
-operand_list = list(operand_string)
+    #create a variable to store the output of the loop.
+    output_string = ""
 
-#print for debugging
-#print(operand_list)
-
-#create variable to store final result (or for functions to append to it midway)
-output_string = ""
-
-
-#function purpose: if the count is greater than 0, append the letter and the number of times it appears to the output string
-def count_a_letter(current_letter_of_the_alphabet):
-    #for each letter in the alphabet, check to see if the letter is in the operand string
-    #if it is, append the letter and the number to the output string
-    if operand_list.count(current_letter_of_the_alphabet) > 0:
-        #print for debugging
-        print(operand_list.count(current_letter_of_the_alphabet))
+    #print for debugging
+    print(operand_string)
+    print(operand_list)
+   
 
 
+    #for each letter in the alphabet, 
+    for i in range (len(alphabet_container_list)):
+        #check if the letter is found in the input string. If so,
+        if operand_list.count(alphabet_container_list[i]) > 0:
+            #set a variable to zero
+            specific_letter_count = 0
+            #concatenate current letter to output string
+            output_string = output_string + (alphabet_container_list[i])
+            #store value of the count of that letter in the input string
+            specific_letter_count = operand_list.count(alphabet_container_list[i])
+            #concatenate count of the letter (in string form) to the letter being counted
+            output_string = output_string + str(specific_letter_count)
 
-for i in range (len(operand_list)):
-    specific_letter_count = count_a_letter((operand_list[i]))
-    
+    print (output_string)
+
+
+main()
