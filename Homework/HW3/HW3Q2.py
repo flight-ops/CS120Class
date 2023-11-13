2958d7c9261bef313f9a4ed7d8c9859c4a6999ae
flight-ops <ezhou6@gmail.com>
2023-11-12 21:38:33 -0500

    more complete homework

--- Homework/HW3/HW3Q2.py
+++ Homework/HW3/HW3Q2.py
@@ -8,6 +8,9 @@
 #department manager can use get_a_student_in_sport(), #view_all_student_in_sport()
 
 #student can have a maximum of three sports
+
+import random
+
 class Department:
     def __init__(self) -> None:
         pass
@@ -21,41 +24,85 @@ class Student:
         return self.name
     
     def add_sport(self,sport_to_add):
-        
+        #append a sport to student's sports list
         self.sports_enrolled.append(sport_to_add)
-
+     
     def switch_sport(self,sport_to_add,sport_to_drop):
+        #grab index of sport that will be removed
+        swap_index = self.sports_enrolled.index(sport_to_drop)
+        #remove sport
         self.sports_enrolled.remove(sport_to_drop)
-        self.sports_enrolled.append(sport_to_add)
+        #insert new sport using the saved index
+        self.sports_enrolled.insert(swap_index, sport_to_add)
 
     def drop_sport(self,sport_to_drop):
         self.sports_enrolled.remove(sport_to_drop)
 
 class Department_Manager:
-    def __init__(self) -> None:
-        pass
-    def get_student_in_sport(self):
-        pass
-    def view_all_in_sport(self):
-        pass
-#create a function to limit the number of sports a student can be in
-def sport_count_checker(student_object):
-    pass
+    def __init__(self,name) -> None:
+        self.name = name
+    def get_student_in_sport(self,student_dict,sport_query):
+        get_rand_student_in_sport(student_dict,sport_query)
+    def get_all_in_sport(self,student_dict,sport_query):
+        get_all_student_in_sport(student_dict,sport_query)
 
-def manager_actions():
-    manager_action_request = input("")
+
+def manager_actions(student_dict):
+    quit_condition = False
+    while quit_condition == False:
+        manager_action_request = input("What would you like to do? Options are: \nget a student in sport (will be a random one), get all students in a sport, or quit.\n")
+        manager_action_request = manager_action_request.lower()
+        #let the 
+        if manager_action_request == "get a student in sport":
+            sport_query = input("What sport do you want to search with?\n")
+            get_rand_student_in_sport(student_dict,sport_query)
+        if manager_action_request == "get all students in a sport":
+            sport_query = input("What sport do you want to search with?\n")
+            get_all_student_in_sport(student_dict,sport_query)
+        #let the user quit
+        elif manager_action_request == "quit":
+            quit_condition = True
+        else:
+            print("Unknown input. Try again.")
+
+    
 def student_actions(student_identity):
     print (student_identity)
     quit_condition = False
     while quit_condition == False:
-        student_action_request = input("What would you like to do? Options are: \nadd a sport, switch sports, drop a sport, or quit.\n")
+        student_action_request = input("What would you like to do? Options are: \nadd a sport, switch a sport, drop a sport, or quit.\n")
         student_action_request = student_action_request.lower()
-
+        #let the user add a sport
         if student_action_request == "add a sport":
-            add_request = input("What sport would you like to add?\n")
-            student_identity.add_sport(add_request)
-            print("Adding sport...")
-            print(f"Current student: {str(student_identity)} \nAdded sport: {add_request}")
+            sport_count = 0
+            #count the sports in student attribute
+            for i in student_identity.sports_enrolled:
+                sport_count+=1
+
+            #if less than max, let user add another sport
+            if sport_count <3:
+                add_request = input("What sport would you like to add?\n")
+                print("Adding sport...")
+                student_identity.add_sport(add_request)
+                print(f"Current student: {str(student_identity)} \nAdded sport: {add_request}")
+
+                #three sport max condition
+            else:
+                print("\nError: student can only have maximum of three sports. try switching or removing a sport.\n")
+        #let the user drop a sport
+        elif student_action_request == "drop a sport":
+            drop_request = input("What sport would you like to drop?")
+            print("Dropping sport...")
+            student_identity.drop_sport(drop_request)
+            print(f"Current student: {str(student_identity)} \nDropped sport: {drop_request}")
+        #let the user switch a sport
+        elif student_action_request == "switch a sport":
+            drop_request = input("What sport would you like to switch out?\n")
+            add_request = input("What sport would you like to switch in?\n")
+            print("Switching sports.")
+            student_identity.switch_sport(add_request,drop_request)
+            print(f"Current student: {str(student_identity)} \nSwitched {drop_request} with {add_request}.")
+        #let the user quit
         elif student_action_request == "quit":
             quit_condition = True
         else:
@@ -69,7 +116,7 @@ def identity_checker(student_dict):
         user_identity = input("Are you a Student or Department Manager? Alteratively, quit.\n")
         user_identity = user_identity.lower()
         if user_identity == "department manager":
-            manager_actions()
+            manager_actions(student_dict)
             
         elif user_identity == "student":
             student_ident = input("What is your name? (test w/ student1, student2, student3)\n")
@@ -80,18 +127,32 @@ def identity_checker(student_dict):
         else:
             print("Error. Try again.")
 
-            
+def get_rand_student_in_sport(student_dict,sport_query):
+    return_list = []
+    for student in student_dict:
+        pass
+
+def get_all_student_in_sport(student_dict,sport_query):
+    return_list = []
+    for student in student_dict:
+        if sport_query in student_dict[student].sports_enrolled:
+            return_list.append(student)
+    print (return_list)
 
     
     
 
 def main():
-    student1 = Student("student1",["Basketball"])
+
+    student1 = Student("student1",["Basketball","Baseball","Volleyball"])
     student2 = Student("student2",["Basketball"])
     student3 = Student("student3",["Volleyball"])
 
     student_dict = {student1.name:student1, student2.name:student2, student3.name:student3}
+    print(" --- important ---")
+    print (" --- sport names are case sensitive ---")
     identity_checker(student_dict)
+    print(student1.sports_enrolled)
 
 
 
--- Projects/OOP miniproject2.py
+++ Projects/OOP miniproject2.py
@@ -13,22 +13,81 @@
 
 #assume there are 10 courses, 20 students, and 5 teachers
 
+class school:
+    def __init__(self):
+        pass
+
+    #method to hire a teacher given a name and a list of courses
+    def hire_teacher(self, teacher_name, courses_list):
+        pass
+
+    def enroll_student(self, student_name, report_dict):
+        pass
+
+class school_record:
+    def __init__(self):
+        pass
+
+    
+
 class teacher:
-    def __init__(self, teacher_name, course_IDs) -> None:#insert data structure here
+    def __init__(self, teacher_name, course_IDs) -> None:
         self.teacher_name = teacher_name
         self.course_IDs = course_IDs
 
 class student:
-    def __init__(self, student_name, course_IDs, course_grades) -> None:
+    def __init__(self, student_name, report_dict) -> None:
         self.student_name = student_name
-        self.course_IDs = course_IDs
-        self.course_grades = course_grades
+        self.student_report = report_dict
+
 
 
 def write_to_log():
     pass
 
-def main():
-    pass
+student_1 = student("Timmy",report_dict= {"Course1":"A","Course2":"B"})
+student_2 = student("Tommy",report_dict={"Course1":"F","Course2":"C"})
+student_3 = student("Tammy",report_dict={"Course3":"A","Course1":"B"})
+student_dict = [student_1,student_2,student_3]
 
+#search for a student in a certain course
+def search_by_course(course_query):
+    returnlist = []
+    for student in student_dict:
+        if course_query in student.student_report:
+            returnlist.append(student.student_name)
+    print(returnlist)
+
+def search_by_grade(grade_query):
+    returnlist = []
+    for student in student_dict:
+        print (student.student_report)
+        for course in student.student_report:
+            if student.student_report[course] == grade_query:
+                returnlist.append(student.student_name)
+    print(returnlist)
+
+
+
+                
+
+def search_by_course_and_grade(course_query,grade_query):
+    returnlist = []
+
+    for student in student_dict:
+        #for each student, check if the student is in the specified class
+        if course_query in student.student_report:
+            print(student.student_name)
+            #check if that student has the specified grade for the specified class
+            if student.student_report[course_query] == grade_query:
+                returnlist.append(student.student_name)
+
+    #if a value exists
+    if returnlist != []:
+        print(returnlist)
+    elif returnlist == []:
+        print("Error: no students matching parameters")
+
+def main():
+    search_by_course_and_grade("Course1","B")
 main()
